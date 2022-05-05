355ba2cc199d49ae121597e58334eac1ac4b53b7
suharbramar <suhar_bramar@yahoo.com>
2022-05-05 23:55:05 +0700

    manage multiple config yaml file

--- src/main/java/com/bramcode/springbootprofile/SpringbootprofileApplication.java
+++ src/main/java/com/bramcode/springbootprofile/SpringbootprofileApplication.java
@@ -1,13 +1,24 @@
 package com.bramcode.springbootprofile;
 
+import com.bramcode.springbootprofile.config.YamlConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootprofileApplication {
+public class SpringbootprofileApplication implements CommandLineRunner {
+
+	@Autowired
+	private YamlConfig yamlConfig;
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringbootprofileApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(yamlConfig);
+	}
 }
---
+++ src/main/java/com/bramcode/springbootprofile/config/YamlConfig.java
@@ -0,0 +1,69 @@
+package com.bramcode.springbootprofile.config;
+
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+@EnableConfigurationProperties
+@ConfigurationProperties("server")
+public class YamlConfig {
+    private String email;
+    private List<Cluster> cluster;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Cluster> getCluster() {
+        return cluster;
+    }
+
+    public void setCluster(List<Cluster> cluster) {
+        this.cluster = cluster;
+    }
+
+    @Override
+    public String toString() {
+        return "YamlConfig{" +
+                "email='" + email + '\'' +
+                ", cluster=" + cluster +
+                '}';
+    }
+
+    public static class Cluster{
+        private String ip;
+        private String path;
+
+        public String getIp() {
+            return ip;
+        }
+
+        public void setIp(String ip) {
+            this.ip = ip;
+        }
+
+        public String getPath() {
+            return path;
+        }
+
+        public void setPath(String path) {
+            this.path = path;
+        }
+
+        @Override
+        public String toString() {
+            return "Cluster{" +
+                    "ip='" + ip + '\'' +
+                    ", path='" + path + '\'' +
+                    '}';
+        }
+    }
+}
