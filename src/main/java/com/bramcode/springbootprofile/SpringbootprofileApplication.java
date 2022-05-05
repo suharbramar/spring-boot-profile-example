ae83ac533db48da20df5fdfd20edc876f54275f9
suharbramar <suhar_bramar@yahoo.com>
2022-05-05 19:58:52 +0700

    Initial commit

---
+++ src/main/java/com/bramcode/springbootprofile/SpringbootprofileApplication.java
@@ -0,0 +1,13 @@
+package com.bramcode.springbootprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootprofileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootprofileApplication.class, args);
+	}
+
+}
